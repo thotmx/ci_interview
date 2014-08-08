a57820c4665cda7c8ecb69f4225368b4b2efb0c1
hermes-logicalbricks <hermes.ojeda@logicalbricks.com>
2014-08-08 12:25:28 -0500

    Added cell to world on constructor

--- world_dirty_test.rb
+++ world_dirty_test.rb
@@ -1,11 +1,18 @@
 require './dirty_tester'
 require './world'
+require './cell'
 
 class WorldDirtyTester < DirtyTester
 
   def create_empty_world
     world = World.new
     raise "Fail" unless world.number_of_cells.number == 0
+  end
+
+  def create_world_with_one_cell
+    world = World.new(Cell.new)
+
+    raise "Fail" unless world.number_of_cells.number == 1
   end
 
 end
