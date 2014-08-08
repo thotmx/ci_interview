bb7a2f7bf5b8f6546cc2363c98c8bdf2ae3e5915
hermes-logicalbricks <hermes.ojeda@logicalbricks.com>
2014-08-08 11:13:10 -0500

    Fixing call to run methods

--- gof.rb
+++ gof.rb
@@ -9,7 +9,6 @@ class Cell
 end
 
 class DirtyTester
-
   def test_cell_creation
     cell = Cell.new
     raise "Fail" if cell.nil?
@@ -20,8 +19,14 @@ class DirtyTester
     raise "Fail" unless cell.alive?
   end
 
+  def die_cell_not_alive
+    cell = Cell.new
+    cell.die
+    raise "Fail" if cell.alive?
+  end
+
   def run
-    methods(false).each do |method|
+    self.class.instance_methods(false).each do |method|
       send(method) unless method == :run
     end
   end
