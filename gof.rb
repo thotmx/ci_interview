582a77830a508077721d941bf5bbf10fdd90afa5
hermes-logicalbricks <hermes.ojeda@logicalbricks.com>
2014-08-08 11:01:47 -0500

    Starting testing cell alive

--- gof.rb
+++ gof.rb
@@ -1,4 +1,7 @@
 class Cell
+  def alive?
+    false
+  end
 end
 
 
@@ -7,6 +10,11 @@ def test_cell_creation
   raise "Fail" if cell.nil?
 end
 
+def create_cell_alive
+  cell = Cell.new
+  raise "Fail" unless cell.alive?
+end
 
 
 test_cell_creation
+create_cell_alive
